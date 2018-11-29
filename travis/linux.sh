75f9784c1417ea737684a08ebe6977ffaa852a67
Ernesto Castellotti <erny.castell@gmail.com>
2018-11-29 21:08:54 +0100

    Update linux.sh

--- travis/linux.sh
+++ travis/linux.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Build deps
+sudo add-apt-repository ppa:openjdk-r/ppa
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get upgrade
-sudo apt-get install gcc-8 g++-8 make gperf openjdk-8-jdk cmake libssl-dev
+sudo apt-get install gcc-8 g++-8 make gperf openjdk-11 cmake libssl-dev
 
 # Dirs
 cd src/main/jni
